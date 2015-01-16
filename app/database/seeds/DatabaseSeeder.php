bb50de348ce10da03b4c140e0fd0b011013d4544
aranna00 <aranna00@gmail.com>
2015-01-16 16:05:03 +0100

    Dealer table seeds

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder {
 		$this->call('GroupsTableSeeder');
 		$this->call('UsersTableSeeder');
 		$this->call('CarsTableSeeder');
+		$this->call('DealerTableSeeder');
 	}
 
 }
--- app/database/seeds/DealerTableSeeder.php
+++ app/database/seeds/DealerTableSeeder.php
@@ -1,20 +1,17 @@
 <?php
 
-// Composer: "fzaninotto/faker": "v1.3.0"
-use Faker\Factory as Faker;
-
 class DealerTableSeeder extends Seeder {
 
 	public function run()
 	{
-		$faker = Faker::create();
-
-		foreach(range(1, 10) as $index)
-		{
-			Dealer::create([
-
-			]);
-		}
+		Dealer::create(['name'=>'Brederode']);
+		Dealer::create(['name'=>'Schaap']);
+		Dealer::create(['name'=>'Jacobsen']);
+		Dealer::create(['name'=>'Mudde']);
+		Dealer::create(['name'=>'Orsouw']);
+		Dealer::create(['name'=>'Wensink']);
+		Dealer::create(['name'=>'Jansen']);
+		Dealer::create(['name'=>'Duursma']);
 	}
 
 }
