a875bfd5121ac477195dada09fb97cbb5d676a90
aranna00 <aranna00@gmail.com>
2015-01-16 10:07:19 +0100

    session access moved to correct file

--- app/start/global.php
+++ app/start/global.php
@@ -95,36 +95,3 @@ require app_path().'/filters.php';
 	{
 		Cache::put('userGroup', Sentry::findGroupByName('user'), 1440);
 	}
-	if(!Session::has('adminAccess'))
-	{
-		if(Sentry::getUser()->inGroup(Cache::get('adminGroup')))
-		{
-			Session::put('adminAccess', true);
-		}
-		else
-		{
-			Session::put('adminAccess', false);
-		}
-	}
-	if(!Session::has('adminAccess'))
-	{
-		if(Sentry::getUser()->inGroup(Cache::get('dealerGroup')))
-		{
-			Session::put('dealerAccess', true);
-		}
-		else
-		{
-			Session::put('dealerAccess', true);
-		}
-	}
-	if(!session::has('userAccess'))
-	{
-		if(Sentry::getUser()->inGroup(Cache::get('userGroup')))
-		{
-			Session::put('userAccess', true);
-		}
-		else
-		{
-			Session::put('dealerAccess', true);
-		}
-	}
--- app/start/local.php
+++ app/start/local.php
@@ -1,3 +1,35 @@
 <?php
 
-//
+	if(!Session::has('adminAccess'))
+	{
+		if(Sentry::getUser()->inGroup(Cache::get('adminGroup')))
+		{
+			Session::put('adminAccess', true);
+		}
+		else
+		{
+			Session::put('adminAccess', false);
+		}
+	}
+	if(!Session::has('adminAccess'))
+	{
+		if(Sentry::getUser()->inGroup(Cache::get('dealerGroup')))
+		{
+			Session::put('dealerAccess', true);
+		}
+		else
+		{
+			Session::put('dealerAccess', true);
+		}
+	}
+	if(!session::has('userAccess'))
+	{
+		if(Sentry::getUser()->inGroup(Cache::get('userGroup')))
+		{
+			Session::put('userAccess', true);
+		}
+		else
+		{
+			Session::put('dealerAccess', true);
+		}
+	}
