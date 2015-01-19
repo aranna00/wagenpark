5825b2f52cd70850a9c655b0c8a6c021252ecb84
aranna00 <aranna00@gmail.com>
2015-01-19 12:13:50 +0100

    Migration for fk cars->dealer

---
+++ app/database/migrations/2015_01_19_111059_add_dealer_fk_to_cars_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddDealerFkToCarsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('cars', function(Blueprint $table)
+		{
+			$table->foreign('dealer_id')
+			      ->references('id')
+			      ->on('dealers');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('cars', function(Blueprint $table)
+		{
+			$table->dropForeign('car_dealer_id_foreign');
+		});
+	}
+
+}
